4a92eab9d23b89088ecaa4f8df5e39b9142694b7
Ghavvvo <2gaherre12@gmail.com>
2026-04-04 23:28:53 -0400

    feat : adding navbar to privacy section

--- src/modules/client/pages/PrivacyPolicy.tsx
+++ src/modules/client/pages/PrivacyPolicy.tsx
@@ -1,6 +1,15 @@
+import Navbar from "@/modules/shared/components/Navbar";
+import Footer from "@/modules/shared/components/Footer";
 import PrivacyPolicySection from '@/modules/client/components/PrivacyPolicySection';
 
 export default function PrivacyPolicy() {
-  return <PrivacyPolicySection />;
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <main className="pt-24">
+        <PrivacyPolicySection />
+      </main>
+      <Footer />
+    </div>
+  );
 }
-
