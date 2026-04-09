7cd804a10f43cdea292eb55888bc151d05b173ff
Ghavvvo <2gaherre12@gmail.com>
2026-04-09 15:15:57 -0400

    Revert "feat : commenting stripe paying method"
    This reverts commit f8c9c25d7e9c9f17d00bff3b2cd9b20bc73489e1.

--- src/modules/client/components/reservation/payment/PaymentMethodsStep.tsx
+++ src/modules/client/components/reservation/payment/PaymentMethodsStep.tsx
@@ -40,9 +40,7 @@ export default function PaymentMethodsStep({ hook, totalPrice }: PaymentMethodsS
           onClick={() => hook.goToStep('payment-bizum')}
         />
 
-
-        {/* Stripe
-
+        {/* Stripe */}
         <PaymentMethodButton
           label="Stripe"
           description={t("reservation.stripeDescription")}
@@ -50,7 +48,7 @@ export default function PaymentMethodsStep({ hook, totalPrice }: PaymentMethodsS
           onClick={() => submitPayment('stripe')}
           disabled={submitting}
           highlighted
-        />*/}
+        />
 
         {/* PayPal */}
         <PayPalPaymentMethod hook={hook} />
